e940e1e062d39bccb038b6b93c2ddcb840a07778
javierhol <javierholguin596@gmail.com>
2023-02-19 21:32:54 -0500

    Backend in Django with Mongodb

---
+++ administrationManagement/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdministrationmanagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'administrationManagement'
